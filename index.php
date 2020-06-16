73e715940012c7b9b739a4634b9bb23651eef6bd
Marco <marco.faltoni@outlook.it>
2020-06-16 14:50:13 +0200

    Inizio

---
+++ index.php
@@ -0,0 +1,27 @@
+<!-- Create un array per salvare tutte le faq che trovate in questa pagina:
+https://policies.google.com/faq
+Stampate in pagina tutte le domande dinamicamente con un ciclo in php. -->
+
+<?php
+
+$faq = []
+
+?>
+
+
+<!-- <!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Google FAQ</title>
+</head>
+
+<body>
+    <div id="container-faq">
+
+
+    </div>
+</body>
+
+</html> -->
